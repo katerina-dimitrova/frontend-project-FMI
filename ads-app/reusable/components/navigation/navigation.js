9390295cd124f84b87ac2310f62eb34090094dc0
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2025-01-27 10:30:35 +0200

    Add new reusable component - navigation

---
+++ ads-app/reusable/components/navigation/navigation.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("../../reusable/components/navigation/navigation.html")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((data) => {
+        document.getElementById("navbar").innerHTML = data;
+  
+        const menuToggle = document.getElementById("menu-toggle");
+        const navLinks = document.querySelector(".nav-links");
+  
+        menuToggle.addEventListener("click", () => {
+          navLinks.classList.toggle("active");
+        });
+      })
+      .catch((error) => {
+        console.error("Error loading navbar:", error);
+      });
+  });
+  
