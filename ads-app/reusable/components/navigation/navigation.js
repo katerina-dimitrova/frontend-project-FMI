f8fd9682d8464f5a9d3c3625446f9f157e71a63e
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2025-01-30 23:18:25 +0200

    Add logout functionality to profile icon in navigation

--- ads-app/reusable/components/navigation/navigation.js
+++ ads-app/reusable/components/navigation/navigation.js
@@ -1,23 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("../../reusable/components/navigation/navigation.html")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.text();
-      })
-      .then((data) => {
-        document.getElementById("navbar").innerHTML = data;
-  
-        const menuToggle = document.getElementById("menu-toggle");
-        const navLinks = document.querySelector(".nav-links");
-  
-        menuToggle.addEventListener("click", () => {
-          navLinks.classList.toggle("active");
-        });
-      })
-      .catch((error) => {
-        console.error("Error loading navbar:", error);
+  fetch("../../reusable/components/navigation/navigation.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+
+      const menuToggle = document.getElementById("menu-toggle");
+      const navLinks = document.querySelector(".nav-links");
+
+      menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
       });
-  });
-  
+
+      const logout = document.getElementById("profile");
+      logout.addEventListener("click", () => {
+        localStorage.removeItem("isAuthenticated");
+        window.location.href = "../../features/auth/login.html";
+      });
+    })
+    .catch((error) => {
+      console.error("Error loading navbar:", error);
+    });
+});
