a864e2332a5610896404dba80f525364476ebb57
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2025-01-31 21:21:00 +0200

    Update token storage to include user email

--- ads-app/features/auth/login.js
+++ ads-app/features/auth/login.js
@@ -64,7 +64,7 @@ function handleMainAction() {
     }
   }
 
-  localStorage.setItem("token", JSON.stringify({isAuthenticated: true}));
+  localStorage.setItem("token", JSON.stringify({user: email, isAuthenticated: true}));
   window.location.href = "../../features/home/home.html";
 }
 
