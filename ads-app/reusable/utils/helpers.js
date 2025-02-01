9afa9da1d8f080dd1f4abf286d21e9c8530be9f8
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2025-02-01 23:56:45 +0200

    Enhance createAd function

--- ads-app/reusable/utils/helpers.js
+++ ads-app/reusable/utils/helpers.js
@@ -1,3 +1,5 @@
+import { CATEGORIES } from "./constants";
+
 export function createUser(email, password) {
     if (!email || !password) {
       throw new Error("Email and password are required");
@@ -9,16 +11,21 @@ export function createUser(email, password) {
     };
   }
 
-export function createAd(userEmail, title, description, price, image = "../../assets/images/default_ad_image.jpg") {
-    if (!title || !description || !price || !image) {
-      throw new Error("Title, description, price, and image are required");
+export function createAd(userEmail, title, description, price, category, image = "../../assets/images/default_ad_image.jpg") {
+    if (!title || !description || !price || !image || !category) {
+      throw new Error("Title, description, price, category and image are required");
     }
+
+    if (!Object.values(CATEGORIES).includes(category)) {
+      throw new Error(`Invalid category: ${category}. Allowed categories: ${Object.values(CATEGORIES).join(", ")}`);
+  }
   
     return {
       userEmail,
       title,
       description,
       price,
+      category,
       image,
     };
   }
--- ads-app/reusable/utils/repository.js
+++ ads-app/reusable/utils/repository.js
@@ -1,12 +1,13 @@
 import { createUser, createAd } from "./helpers.js";
+import { CATEGORIES } from "./constants.js";
 
 const defaultAds = [
-  createAd("ads@gmail.com", "Example add 1", "Description", "50"),
-  createAd("ads@gmail.com", "Example add 2", "Description", "100"),
-  createAd("ads@gmail.com", "Example add 3", "Description.", "150"),
-  createAd("ads@gmail.com", "Example add 4", "Description", "150"),
-  createAd("ads@gmail.com", "Example add 5", "Description", "150"),
-  createAd("ads@gmail.com", "Example add 6", "Description", "150"),
+  createAd("ads@gmail.com", "Example add 1", "Description", "50", CATEGORIES.ELECTRONICS),
+  createAd("ads@gmail.com", "Example add 2", "Description", "100", CATEGORIES.VEHICLES),
+  createAd("ads@gmail.com", "Example add 3", "Description.", "150", CATEGORIES.REAL_ESTATE),
+  createAd("ads@gmail.com", "Example add 4", "Description", "150", CATEGORIES.FASHION),
+  createAd("ads@gmail.com", "Example add 5", "Description", "150", CATEGORIES.HOME_APPLIANCES),
+  createAd("ads@gmail.com", "Example add 6", "Description", "150", CATEGORIES.SPORTS),
 ];
 
 const users = [
