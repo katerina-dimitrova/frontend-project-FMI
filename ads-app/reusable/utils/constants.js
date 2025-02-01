ced0d5d11d3ed01aae74d7be8b9014c6d1d37f20
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2025-02-01 23:54:35 +0200

    Add constants

---
+++ ads-app/reusable/utils/constants.js
@@ -0,0 +1,9 @@
+export const CATEGORIES = {
+    ELECTRONICS: "Electronics",
+    VEHICLES: "Vehicles",
+    REAL_ESTATE: "Real estate",
+    FASHION: "Fashion",
+    HOME_APPLIANCES: "Home appliances",
+    SPORTS: "Sports",
+    OTHERS: "Others",
+};
