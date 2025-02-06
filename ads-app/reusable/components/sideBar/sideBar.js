31883bb33d40a7119d1c070c6b13427cafab6792
Katerina Dimitrova <katerina.dimitrova440@gmail.com>
2025-02-06 17:50:05 +0200

    Add balance and history pages

--- ads-app/reusable/components/sideBar/sideBar.js
+++ ads-app/reusable/components/sideBar/sideBar.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const sidebarContainer = document.getElementById("sidebar-container");
+  const sidebarContainer = document.getElementById("sidebar-container");
 
-    if (sidebarContainer) {
-        fetch("../../reusable/components/sideBar/sideBar.html")
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Sidebar not found!");
-                }
-                return response.text();
-            })
-            .then(data => {
-                sidebarContainer.innerHTML = data;
-            })
-            .catch(error => console.error("Error loading sidebar:", error));
-    }
+  if (sidebarContainer) {
+    fetch("../../reusable/components/sideBar/sideBar.html")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Sidebar not found!");
+        }
+        return response.text();
+      })
+      .then((data) => {
+        sidebarContainer.innerHTML = data;
+
+        const logout = document.getElementById("logout");
+        logout.addEventListener("click", () => {
+          localStorage.removeItem("token");
+          window.location.href = "../../features/auth/login.html";
+        });
+      })
+      .catch((error) => console.error("Error loading sidebar:", error));
+  }
 });
